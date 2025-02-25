ecc0f21cd6aacff628b23cc082d85d14591c5e4c
Omer Feldman <omerf@MacBookPro.lan>
2025-02-25 13:59:38 +0200

    remove redundant service plan

--- IaC/infra/main.tf
+++ IaC/infra/main.tf
@@ -3,14 +3,6 @@ resource "azurerm_resource_group" "demo" {
   location = var.region
 }
 
-resource "azurerm_service_plan" "demo" {
-  name                = "${var.base_name}-serviceplan"
-  location            = var.region
-  resource_group_name = azurerm_resource_group.demo.name
-  os_type             = "Linux"
-  sku_name            = "B2"
-}
-
 resource "azurerm_container_app_environment" "demo" {
   name                = "${var.base_name}-app-environment"
   location            = var.region
