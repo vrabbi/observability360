217438de09f21482ccc2cea049ee30a4bf4ea334
“vladfeigin” <vladfeigin@microsoft.com>
2025-02-28 08:36:55 +0200

    refactoring adding new services

--- online_store/ui/online_store_ui.py
+++ online_store/ui/online_store_ui.py
@@ -4,15 +4,16 @@ from dotenv import load_dotenv
 import streamlit as st
 from user_ui import run_user_ui
 from product_ui import run_product_ui
+from cart_ui import run_cart_ui
 
 load_dotenv()
 
 st.set_page_config(page_title="Online Store UI", layout="wide")
-st.title("Online Store Dashboard")
+st.title("Online Store. Observability Demo.")
 
 # Sidebar navigation: select a service section
 service = st.sidebar.radio("Select Service", 
-                           ["Home", "User Service", "Product Service", "Order Service", "Cart Service"])
+                           ["Home", "User Service", "Product Service", "Cart Service", "Order Service", ])
 
 if service == "Home":
     st.header("Welcome")
@@ -21,4 +22,6 @@ elif service == "User Service":
     run_user_ui()
 elif service == "Product Service":
     run_product_ui()
+elif service == "Cart Service":
+    run_cart_ui()
     
