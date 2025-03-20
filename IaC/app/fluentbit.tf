778e5b76e7df69ddf146dd256df7e1eb5c176b21
Omer Feldman <omerfeldman@Mac.lan>
2025-03-20 14:31:41 +0200

    added

--- IaC/app/fluentbit.tf
+++ IaC/app/fluentbit.tf
@@ -9,6 +9,43 @@ resource "kubernetes_namespace" "fluentbit" {
   }
 }
 
+resource "kubernetes_service_account" "fluentbit" {
+  metadata {
+    name      = "fluent-bit"
+    namespace = kubernetes_namespace.fluentbit.metadata[0].name
+  }
+}
+
+resource "kubernetes_cluster_role" "fluentbit" {
+  metadata {
+    name = "fluent-bit-read"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["namespaces", "pods"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+
+resource "kubernetes_cluster_role_binding" "fluentbit" {
+  metadata {
+    name = "fluent-bit-read"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "fluent-bit-read"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "fluent-bit"
+    namespace = kubernetes_namespace.fluentbit.metadata[0].name
+  }
+}
 
 resource "kubernetes_config_map" "fluentbit" {
   metadata {
@@ -21,79 +58,80 @@ resource "kubernetes_config_map" "fluentbit" {
       otel_collector_service_name   = kubernetes_service.otel_collector.metadata[0].name,
       otel_collector_namespace_name = kubernetes_namespace.opentelemtry.metadata[0].name
     })
+    "parsers.conf" = file("${path.cwd}/${local.fluentbit_directory_path}/parsers.conf")
   }
 }
 
-# resource "kubernetes_daemonset" "fluentbit" {
-#   metadata {
-#     name      = "fluent-bit"
-#     namespace = kubernetes_namespace.fluentbit.metadata[0].name
-#     labels = {
-#       app = "fluent-bit"
-#       "kubernetes.io/cluster-service" : "true"
-#     }
-#   }
+resource "kubernetes_daemonset" "fluentbit" {
+  metadata {
+    name      = "fluent-bit"
+    namespace = kubernetes_namespace.fluentbit.metadata[0].name
+    labels = {
+      app = "fluent-bit"
+      "kubernetes.io/cluster-service" : "true"
+    }
+  }
 
+  spec {
+    selector {
+      match_labels = {
+        app = "fluent-bit"
+      }
+    }
 
-#   spec {
-#     selector {
-#       match_labels = {
-#         app = "fluent-bit"
-#       }
-#     }
+    template {
+      metadata {
+        labels = {
+          app = "fluent-bit"
+          "kubernetes.io/cluster-service" : "true"
+        }
+      }
 
-#     template {
-#       metadata {
-#         labels = {
-#           app = "fluent-bit"
-#           "kubernetes.io/cluster-service" : "true"
-#         }
-#       }
+      spec {
+        service_account_name = kubernetes_service_account.fluentbit.metadata[0].name
 
-#       spec {
-#         container {
-#           name  = "fluent-bit"
-#           image = "fluent/fluent-bit"
+        container {
+          name  = "fluent-bit"
+          image = "fluent/fluent-bit"
 
-#           volume_mount {
-#             name       = "fluent-bit-config"
-#             mount_path = "/fluent-bit/etc/"
-#           }
-#           volume_mount {
-#             name       = "varlog"
-#             mount_path = "/var/log/"
-#           }
-#             volume_mount {
-#                 name       = "varlibdockercontainers"
-#                 mount_path = "/var/lib/docker/containers/"
-#                 read_only  = true
-#             }
-#         }
+          volume_mount {
+            name       = "fluent-bit-config"
+            mount_path = "/fluent-bit/etc/"
+          }
+          volume_mount {
+            name       = "varlog"
+            mount_path = "/var/log/"
+          }
+          volume_mount {
+            name       = "varlibdockercontainers"
+            mount_path = "/var/lib/docker/containers/"
+            read_only  = true
+          }
+        }
 
-#         volume {
-#           name = "fluent-bit-config"
+        volume {
+          name = "fluent-bit-config"
 
-#           config_map {
-#             name = kubernetes_config_map.fluentbit.metadata[0].name
-#           }
-#         }
+          config_map {
+            name = kubernetes_config_map.fluentbit.metadata[0].name
+          }
+        }
+        volume {
+          name = "varlog"
 
-#         volume {
-#           name = "varlog"
+          host_path {
+            path = "/var/log"
+          }
+        }
 
-#           host_path {
-#             path = "/var/log"
-#           }
-#         }
+        volume {
+          name = "varlibdockercontainers"
 
-#         volume {
-#           name = "varlibdockercontainers"
-
-#           host_path {
-#             path = "/var/lib/docker/containers"
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+          host_path {
+            path = "/var/lib/docker/containers"
+          }
+        }
+      }
+    }
+  }
+}
