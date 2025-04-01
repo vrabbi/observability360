7b521ccc2b8d098eef3c667d076bc2e764f0a728
Omer Feldman <omerfeldman@microsoft.com>
2025-04-01 12:00:33 +0300

    aded

--- IaC/app/grafana.tf
+++ IaC/app/grafana.tf
@@ -3,19 +3,25 @@ locals {
   grafana_directory_path = "../../grafana"
 }
 
+resource "time_rotating" "example" {
+  rotation_days = 180
+}
+
 resource "azuread_application" "grafana_to_adx" {
   display_name = "grafana_to_adx"
   owners       = [data.azuread_client_config.current.object_id]
+
+  password {
+    display_name = "${var.base_name}-secret"
+    start_date   = time_rotating.example.id
+    end_date     = timeadd(time_rotating.example.id, "4320h")
+  }
 }
 
 resource "azuread_service_principal" "grafana_to_adx" {
   client_id                    = azuread_application.grafana_to_adx.client_id
   app_role_assignment_required = false
   owners                       = [data.azuread_client_config.current.object_id]
-}
-
-resource "azuread_service_principal_password" "grafana_to_adx" {
-  service_principal_id = azuread_service_principal.grafana_to_adx.id
 }
 
 resource "azurerm_kusto_database_principal_assignment" "grafana_to_adx" {
@@ -28,8 +34,18 @@ resource "azurerm_kusto_database_principal_assignment" "grafana_to_adx" {
   principal_id   = azuread_application.grafana_to_adx.client_id
   principal_type = "App"
   role           = "Viewer"
+}
 
-  depends_on = [azuread_service_principal_password.grafana_to_adx]
+resource "azurerm_role_assignment" "grafana_to_communication_service" {
+  scope                = azurerm_communication_service.demo.id
+  role_definition_name = azurerm_role_definition.communication_service_role.name
+  principal_id         = azuread_service_principal.grafana_to_adx.object_id
+  principal_type = "ServicePrincipal"
+}
+
+resource "azuread_service_principal_password" "grafana_to_adx" {
+  service_principal_id = azuread_service_principal.grafana_to_adx.id
+  depends_on = [ azurerm_kusto_database_principal_assignment.grafana_to_adx ,azurerm_role_assignment.grafana_to_communication_service ]
 }
 
 resource "local_file" "adx_datasource" {
@@ -40,6 +56,22 @@ resource "local_file" "adx_datasource" {
     grafana_client_id     = azuread_application.grafana_to_adx.client_id
     grafana_client_secret = azuread_service_principal_password.grafana_to_adx.value
     tenant_id             = data.azuread_client_config.current.tenant_id
+  })
+}
+
+resource "local_file" "grafana_ini" {
+  filename = "${path.cwd}/${local.grafana_directory_path}/grafana.ini"
+  content = templatefile("${path.cwd}/${local.grafana_directory_path}/grafana.ini.tftpl", {
+    user = "${azurerm_communication_service.demo.name}|${azuread_application.grafana_to_adx.client_id}|${data.azuread_client_config.current.tenant_id}"
+    password = tolist(azuread_application.grafana_to_adx.password).0.value
+    from_address = azurerm_email_communication_service_domain.demo.mail_from_sender_domain
+  })
+}
+
+resource "local_file" "contact_group" {
+  filename = "${path.cwd}/${local.grafana_directory_path}/provisioning/alerting/contact_group.yml"
+  content = templatefile("${path.cwd}/${local.grafana_directory_path}/provisioning/alerting/contact_group.yml.tftpl", {
+    email = var.email
   })
 }
 
@@ -56,7 +88,7 @@ resource "docker_image" "grafana" {
     dir_sha1 = sha1(join("", [for f in fileset(path.cwd, "${local.grafana_directory_path}/**") : filesha1(f)]))
   }
 
-  depends_on = [local_file.adx_datasource]
+  depends_on = [local_file.adx_datasource, local_file.grafana_ini]
 }
 
 resource "docker_registry_image" "grafana" {
